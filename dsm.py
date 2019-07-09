c0255e17109a5d8132b9d4aa996aed3cacbdb3fd
nail.end <julian.endres@rl-institut.de>
2019-07-09 16:20:06 +0200

    cleaned code

--- dsm.py
+++ dsm.py
@@ -31,13 +31,10 @@ def create_model(df_data, timesteps):
 	m.Cup = 40 # Zerrahn Parameter DSM Capacity up(MWh) -> Demand-shift down
 
 	m.DSMdo = Var(m.tm, m.Tm, initialize=0, within=NonNegativeReals)	# Zerrahn Variable load shift down (MWh)
+
 	m.DSMup = Var(m.tm, initialize=0, within=NonNegativeReals)          # Zerrahn Variable load shift up(MWh)
 
 	#################### For OBJECTIVE
-
-	#m.Demand = [80,80,80,80,80,80,80,120,120,120,120,120,120,130,130,120,120,120,120,120,120,80,80,80,80,80,80,80,80,80, 80] #Demand
-
-	#m.Demand = [80,80,80,80,80,80,80,120,120,120,120,120,120,120,120,120,120,120,120,120,120,80,80,80,80,80,80,80,80,80]
 
 	factor_demand = 200
 	temp = df_data.demand_el[:timesteps + 1] * factor_demand
@@ -52,8 +49,6 @@ def create_model(df_data, timesteps):
 
 	m.P1 = Var(m.tm, initialize=0, within=NonNegativeReals) # Power Generator 1 (cheap)
 	m.P2 = Var(m.tm, initialize=0, within=NonNegativeReals) # Power Generator 2 (expensive)
-	#m.P3 = Var(m.tm, initialize=0, within=NonNegativeReals) # Power Gen Backup (super expensive)
-	#m.Wind = Var(m.tm, initialize=0, within=NonNegativeReals)
 
 
 
@@ -135,35 +130,7 @@ def dsmup2_constraint_rule(m, t):
 ####################################################################################
 #                             DEMAND CONSTRAINTS
 
-'''
-def demand_constraint_rule(m, t):
 
-	if t <= m.L:
-		return m.P1[t] + m.P2[t] + m.P3[t] \
-			>= m.Demand[t-1] + m.DSMup[t] - sum(m.DSMdo[T, t] for T in range(1, t+m.L+1))
-
-	elif m.L+1 <= t <= timesteps+1 - m.L:
-		return m.P1[t] + m.P2[t] + m.P3[t] \
-			>= m.Demand[t-1] + m.DSMup[t] - sum(m.DSMdo[T, t] for T in range(t-m.L, t+1+m.L))
-
-	else:
-		return m.P1[t] + m.P2[t] + m.P3[t] \
-			>= m.Demand[t-1] + m.DSMup[t] - sum(m.DSMdo[T, t] for T in range(t-m.L, timesteps+2))
-
-def demand_constraint_rule(m, t):
-
-	if t <= m.L:
-		return m.P1[t] + m.P2[t] + m.P3[t] + m.Wind[t-1] + m.PV[t-1] \
-			>= m.Demand[t-1] + m.DSMup[t] - sum(m.DSMdo[T, t] for T in range(1, t+m.L+1))
-
-	elif m.L+1 <= t <= timesteps+1 - m.L:
-		return m.P1[t] + m.P2[t] + m.P3[t] + m.Wind[t-1] + m.PV[t-1]  \
-			>= m.Demand[t-1] + m.DSMup[t] - sum(m.DSMdo[T, t] for T in range(t-m.L, t+1+m.L))
-
-	else:
-		return m.P1[t] + m.P2[t] + m.P3[t] + m.Wind[t-1] + m.PV[t-1] \
-			>= m.Demand[t-1] + m.DSMup[t] - sum(m.DSMdo[T, t] for T in range(t-m.L, timesteps+2))
- '''
 
 def demand_constraint_rule(m, t):
 
@@ -188,37 +155,15 @@ def power2_constraint_rule(m, t):
 
 	return m.P2[t] <= m.Cap[2]
 
-'''
-def power3_constraint_rule(m, t):
 
-	return m.P3[t] <= m.Cap[3]
-
-
-def powerWind_constraint_rule(m, t, data):
-
-	return m.Wind[t] == m.CapWind * data.wind[t]
-'''
 ###############################################################################
+
 #                                  OBJECTIVE
 
 def obj_expression_cost(m):
 
-	return summation(m.P1) * m.C[1] + summation(m.P2) * m.C[2] #+ summation(m.P3) * m.C[3]
+	return summation(m.P1) * m.C[1] + summation(m.P2) * m.C[2]
 
-
-#########################################################################
-# 					EXTRA OBJECTIVES TRIES
-'''
-def obj_expression_cost_dsm(m):
-
-	return summation(m.P1) * m.C[0] + summation(m.P2) * m.C[1] + summation(m.DSMup) * m.C[2] + summation(m.DSMdo) * m.C[2]
-
-
-def obj_expression_demand(m):
-
-	return summation(m.P1) + summation(m.P2) - summation(m.DSMup) + summation(m.DSMdo)
-
-'''
 ##############################################################################
 # 									HELP PRINT
 
@@ -254,7 +199,6 @@ def output(m):
 	output_DSMup = []
 	output_P1 = []
 	output_P2 = []
-	output_P3 = []
 	output_delay = []
 
 	# Pyomo Var index do start with 1
@@ -278,15 +222,15 @@ def output(m):
 
 	df = pd.DataFrame()
 	df['Demand'] = pd.Series(m.Demand).round()
+
 	df['Wind'] = pd.Series(m.Wind).round()
 	df['PV'] = pd.Series(m.PV).round()
 
 	df['P1'] = pd.Series(output_P1).round()
 	df['P2'] = pd.Series(output_P2).round()
 
-	#df['P3'] = pd.Series(output_P3).round()
-
 	df['DSM_tot'] = (pd.Series(output_DSMup) - pd.Series(output_DSMdo)).round()
+
 	df['DSMdo'] = pd.Series(output_DSMdo).round()
 	df['DSMup'] = pd.Series(output_DSMup).round()
 	df['DSM_delayed'] = pd.Series(output_delay).round()
@@ -307,8 +251,6 @@ def output(m):
 
 	plt.fill_between(range(timesteps+1), 0, df.P1, alpha=0.5,  label='P1', facecolor='black')
 	plt.fill_between(range(timesteps+1), df.P1, df.P2+df.P1, alpha=0.5,  label='P2' , facecolor='grey')
-	#plt.fill_between(range(timesteps + 1), df.P1+df.P2, df.P1 + df.P2 + df.P3, alpha=0.5, label='P3', facecolor='brown')
-
 	plt.fill_between(range(timesteps + 1), df.P1 + df.P2, df.P1 + df.P2 + df.Wind, alpha=0.5, label='Wind',
 					 facecolor='darkcyan')
 	plt.fill_between(range(timesteps + 1), df.P1 + df.P2 + df.Wind, df.P1 + df.P2 + df.Wind + df.PV, alpha=0.5, label='PV',
@@ -376,9 +318,7 @@ m.dsmup2Constraint = Constraint(m.tm, rule=dsmup2_constraint_rule)
 # Power
 m.power1Constraint = Constraint(m.tm, rule=power1_constraint_rule)
 m.power2Constraint = Constraint(m.tm, rule=power2_constraint_rule)
-#m.power3Constraint = Constraint(m.tm, rule=power3_constraint_rule)
 
-#m.powerWindConstraint = Constraint(m.tm, rule=powerWind_constraint_rule)
 
 # Objective
 
